7f28657124662732b2dd188a4a51b85f1427cee1
Allana Yzabelle M. Diaz <allanayzabelle.diaz@wvsu.edu.ph>
2024-04-16 23:45:36 +0800

    changed global css import, initial code

--- src/app/ui/dashboard/sidenav.tsx
+++ src/app/ui/dashboard/sidenav.tsx
@@ -1,5 +1,5 @@
 import NavLinks from '@/app/ui/dashboard/nav-links';
-import '@/app/ui/globals.css'
+import '@/app/ui/global.css'
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2 bg-white rounded-md">
