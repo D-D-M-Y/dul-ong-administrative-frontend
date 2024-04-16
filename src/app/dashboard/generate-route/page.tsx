540530c54eab48dee9e5a02655ce8c9ccd129750
Matthew Montero <99379541+Montero-MD@users.noreply.github.com>
2024-04-16 16:58:32 +0800

    added required fonts with pnpm

--- src/app/dashboard/generate-route/page.tsx
+++ src/app/dashboard/generate-route/page.tsx
@@ -1,41 +1,41 @@
 export default function Home() {
   return (
     <div className="w-dvw h-dvh relative bg-white rounded-[20px] overflow-hidden">
-      <div className="text-neutral-800 text-4xl font-bold font-['Roboto']">Generate Route</div>
+      <div className="text-black text-4xl font-bold font-roboto">Generate Route</div>
 
       <div className="w-full h-fit relative bg-white rounded-[20px]">
         {/* Left Side Content */}
         <div className="w-fit h-fit flex flex-col space-y-6 py-4 px-16">
-          <div className="text-neutral-800 text-2xl font-bold font-['Source Sans Pro']">
+          <div className="text-neutral-800 text-2xl font-bold font-source-sans-pro']">
             Route Overview
           </div>
 
           {/* Package Information */}
           <div className="w-full h-fit flex flex-col border border-zinc-300 border-opacity-50 rounded-[20px] py-4 px-6">
-            <div className="text-neutral-800 text-xl font-normal font-['Source Sans Pro']">
+            <div className="text-neutral-800 text-xl font-normal font-source-sans-pro">
               Total Packages
             </div>
-            <div className="text-neutral-800 text-[52px] font-bold font-['Roboto'] mt-4">
+            <div className="text-neutral-800 text-[52px] font-bold font-roboto mt-4">
               200
             </div>
           </div>
 
           {/* FOOs Available */}
           <div className="w-full flex flex-col border border-zinc-300 border-opacity-50 rounded-[20px] py-4 px-6">
-            <div className="text-neutral-800 text-xl font-normal font-['Source Sans Pro']">
+            <div className="text-neutral-800 text-xl font-normal font-source-sans-pro">
               FOOs Available
             </div>
-            <div className="text-neutral-800 text-[52px] font-bold font-['Roboto'] mt-4">
+            <div className="text-neutral-800 text-[52px] font-bold font-roboto mt-4">
               11
             </div>
           </div>
 
           {/* Routes Constructed */}
           <div className="w-full flex flex-col border border-zinc-300 border-opacity-50 rounded-[20px] py-4 px-6">
-            <div className="text-neutral-800 text-xl font-normal font-['Source Sans Pro']">
+            <div className="text-neutral-800 text-xl font-normal font-source-sans-pro">
               Routes Constructed
             </div>
-            <div className="text-neutral-800 text-[52px] font-bold font-['Roboto'] mt-4">
+            <div className="text-neutral-800 text-[52px] font-bold font-roboto mt-4">
               10
             </div>
           </div>
@@ -44,7 +44,7 @@ export default function Home() {
 
           <button className="w-full h-[50px] left-[62px] top-[580px] absolute">
             <div className="w-[300px] h-[50px] left-0 top-0 absolute bg-indigo-100 rounded-[40px]" />
-            <div className="left-[94px] top-[15px] absolute text-neutral-800 text-base font-bold font-['Roboto']">Generate Route</div>
+            <div className="left-[94px] top-[15px] absolute text-neutral-800 text-base font-bold font-roboto">Generate Route</div>
           </button>
         </div>
 
