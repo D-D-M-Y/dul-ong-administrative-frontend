066a44202d6dc8d67f67fe720f95ba8f93d06fb9
Montero-MD <matthewdavid.montero@wvsu.edu.ph>
2024-04-22 10:38:18 +0800

    no change, just removed duplicate lines by git

--- src/app/generate-route/page.tsx
+++ src/app/generate-route/page.tsx
@@ -5,11 +5,7 @@ export default function Page() {
     <main>
       <h1 className = "font-bold"> Generate Route</h1>
       <div className="w-full h-full relative bg-white rounded-xl">
-      <h1 className = "font-bold"> Generate Route</h1>
-      <div className="w-full h-full relative bg-white rounded-xl">
         {/* Left Side Content */}
-        <div className="w-1/4 h-full flex flex-col space-y-3 p-4 ">
-            <h2 className="font-bold">
         <div className="w-1/4 h-full flex flex-col space-y-3 p-4 ">
             <h2 className="font-bold">
               Route Overview
@@ -20,19 +16,12 @@ export default function Page() {
               <Card title="FOOs Available"              value={"11"}      type="fooavailable" />
               <Card title="Routes Constructed"          value={"10"}      type="routegen" />
             </div>
-
-            <div className="grid gap-6 sm:grid-rows-2 lg:grid-rows-3">
-              <Card title="Total Packages"              value={"200"}     type="total" />
-              <Card title="FOOs Available"              value={"11"}      type="fooavailable" />
-              <Card title="Routes Constructed"          value={"10"}      type="routegen" />
-            </div>
-
+            
           {/* Button */}
             <button className="w-full h-[50px] bg-indigo-100 rounded-[40px] text-neutral-800 text-base font-bold font-roboto py-2">Generate Route</button> 
         </div>
 
         {/* Right Side Image */}
-        <div className="w-3/4 h-full absolute top-0 right-0 rounded-md bg-cover md:bg-contain"
         <div className="w-3/4 h-full absolute top-0 right-0 rounded-md bg-cover md:bg-contain"
           style={{ backgroundImage: "url(https://www.ncgtp.com/NCGTP_Map_Images/map-main-placeholder.jpg)" }}>
         </div>
