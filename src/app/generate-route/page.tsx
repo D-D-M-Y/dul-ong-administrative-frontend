9ab3544e0e6f11b123d901c57e3660c31fe35096
Montero-MD <matthewdavid.montero@wvsu.edu.ph>
2024-04-24 03:26:25 +0800

    leaflet wip

--- src/app/generate-route/page.tsx
+++ src/app/generate-route/page.tsx
@@ -1,28 +1,17 @@
-"use client";
-import React, { useEffect } from 'react';
-import Map from '../components/Map'; // Assuming Map.js is in components folder
 import { Card } from '@/app/ui/dashboard/cards';
-import "leaflet/dist/leaflet.css"
-import "leaflet-defaulticon-compatibility"
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
 
-
+const DEFAULT_CENTER = [38.907132, -77.036546]; // Replace with your desired center coordinates
 
 export default function Page() {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      // Code that uses window object (e.g., react-leaflet initialization)
-    }
-  }, []);
   return (
     <main>
       <h1 className="font-bold"> Generate Route</h1>
       <div className="w-full h-full relative bg-white rounded-xl">
         {/* Left Side Content */}
-        <div className="w-1/4 h-full flex flex-col space-y-3 p-4 ">
-          <h2 className="font-bold">
-            Route Overview
-          </h2>
+        <div className="w-1/4 h-full flex flex-col space-y-3 p-4">
+          <h2 className="font-bold"> Route Overview</h2>
 
           <div className="grid gap-6 sm:grid-rows-2 lg:grid-rows-3">
             <Card title="Total Packages" value={"200"} type="total" />
@@ -30,13 +19,22 @@ export default function Page() {
             <Card title="Routes Constructed" value={"10"} type="routegen" />
           </div>
 
-          {/* Button */}
           <button className="w-full h-[50px] bg-indigo-100 rounded-[40px] text-neutral-800 text-base font-bold font-roboto py-2">Generate Route</button>
         </div>
 
-        {/* Right Side Image */}
+        {/* Right Side Image and Map */}
         <div className="w-2/3 h-auto rounded-r-lg bg-contain md:bg-contain">
-          <Map />
+          <MapContainer center={[38, -77]} zoom={12} style={{ height: '100%' }}> {/* Set map height */}
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={[38, -77]}>
+              <Popup>
+                A simple Leaflet popup.
+              </Popup>
+            </Marker>
+          </MapContainer>
         </div>
       </div>
     </main>
