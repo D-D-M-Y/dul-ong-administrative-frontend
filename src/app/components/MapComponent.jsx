90b02b74aadf311246ba86685fdde79254abd7ac
Montero-MD <matthewdavid.montero@wvsu.edu.ph>
2024-04-30 13:11:25 +0800

    warning status

--- src/app/components/MapComponent.jsx
+++ src/app/components/MapComponent.jsx
@@ -1,39 +1,42 @@
-import React, { useEffect, useState, useRef } from 'react';
-import L from 'leaflet';
-import { TileLayer, MapContainer, LayersControl } from 'react-leaflet';
+"use client";
+import React, { useEffect, useState, useRef } from "react";
+import L from "leaflet";
+import {
+  TileLayer,
+  MapContainer,
+  LayersControl
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-
 // Import the JS and CSS:
-import 'leaflet-routing-machine';
-import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
+import 'leaflet-routing-machine'
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css'
 
 const maps = {
-  base: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  base: "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
 };
 
 const Map = () => {
   const [map, setMap] = useState(null);
 
   // State vars for our routing machine instance:
-  const [routingMachine, setRoutingMachine] = useState(null);
+  const [routingMachine, setRoutingMachine] = useState(null)
 
   // Start-End points for the routing machine:
-  const [start, setStart] = useState([10.7202, 122.5621]);
-  const [end, setEnd] = useState([11.1688, 122.6504]);
+  const [start, setStart] = useState([10.7202, 122.5621]); // Iloilo
+  const [end, setEnd] = useState([11.1688, 122.6504]); // Passi
 
   // Ref for our routing machine instace:
-  const RoutingMachineRef = useRef(null);
+  const RoutingMachineRef = useRef(null)
 
   // Create the routing-machine instance:
   useEffect(() => {
     // Check For the map instance:
-    if (!map) return;
+    if (!map) return
     if (map) {
       // Assign Control to React Ref:
       RoutingMachineRef.current = L.Routing.control({
         position: 'topleft', // Where to position control on map
-        lineOptions: {
-          // Options for the routing line
+        lineOptions: { // Options for the routing line
           styles: [
             {
               color: '#757de8',
@@ -41,28 +44,30 @@ const Map = () => {
           ],
         },
         waypoints: [start, end], // Point A - Point B
-      });
+      })
       // Save instance to state:
-      setRoutingMachine(RoutingMachineRef.current);
+      setRoutingMachine(RoutingMachineRef.current)
     }
-  }, [map]);
+  }, [map])
 
   // Once routing machine instance is ready, add to map:
   useEffect(() => {
-    if (!routingMachine) return;
+    if (!routingMachine) return
     if (routingMachine) {
-      routingMachine.addTo(map);
+      routingMachine.addTo(map)
     }
-  }, [routingMachine]);
+  }, [routingMachine])
+
 
   return (
     <>
       <MapContainer
         center={[10.7202, 122.5621]}
         zoom={13}
-        style={{ height: '100vh', width: '100%', borderRadius: "0 20px 20px 0" }}
+        zoomControl={false}
+        style={{ height: "100vh", width: "100%", borderRadius: "0 20px 0 20px" }}
         // Set the map instance to state when ready:
-        whenCreated={(map) => setMap(map)}
+        whenCreated={map => setMap(map)}
       >
         <LayersControl position="topright">
           <LayersControl.BaseLayer checked name="Map">
