62c34aa5959680778b697487f8fa24711fca7ee3
Allana Yzabelle Diaz <allanayzabelle.diaz@wvsu.edu.ph>
2024-05-21 21:48:31 +0800

    double checked fonts

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
 
         <div className="flex flex-col flex-grow pt-20 pl-20 h-screen">  {/* First column: full width */}
           <img src="/dulong-white.svg" className="w-1/2" />
-          <h1 className="text-white font-source_sans_pro">Made by Ilonggos, for Ilonggos</h1>
-          <p className="text-white font-ptsans font-normal text-xl">A route generating application for the people of Iloilo Province</p>
+          <h1 className="text-white">Made by Ilonggos, for Ilonggos</h1>
+          <p>A route generating application for the people of Iloilo Province</p>
           <img src="/3droute.svg" className="size-9/12 place-self-end" />
         </div>
 
@@ -18,19 +18,19 @@ export default function Page() {
           
           <button className="border rounded-lg p-2 hover:highlight flex justify-center w-1/3">
             <FcGoogle size = {18}/> 
-            <div className = "relative text-center text-lgray text-sm font-ptsans pl-2">Continue with Google</div>
+            <p className = "relative text-center text-textC pl-2">Continue with Google</p>
           </button>
 
           <div className="flex items-center pb-2 mt-4">
             <div className="w-auto p-5 border-b border-lgray py-1"></div>
-            <span className="text-lgray font-source_sans_pro px-2">or</span>
+            <span className="text-lgray font-source-sans-pro px-2">or</span>
             <div className="w-auto p-5 border-b border-lgray py-1"></div>
           </div>
 
           <div className="flex flex-col pt-4 space-y-2 w-1/3">
             <input type="text" id="username" className="border rounded-lg px-2 py-1 text-textC text-m font-ptsans" placeholder="Username or Email" />
             <input type="password" id="password" className="border rounded-lg px-2 py-1 text-textC text-m font-ptsans" placeholder="Password" />
-            <Link href="/login/forgot-password" className="font-source_sans_pro italic text-purple font-bold text-xs text-end underline py-2">Forgot Password?</Link>
+            <Link href="/login/forgot-password" className="font-source-sans-pro italic text-purple font-bold text-xs text-end underline py-2">Forgot Password?</Link>
             <Link href="/"><button className="border rounded-lg p2 w-full bg-indigo-100 rounded-3xl text-center font-bold font-roboto py-3">Login</button> </Link>
           </div>
       </div>
