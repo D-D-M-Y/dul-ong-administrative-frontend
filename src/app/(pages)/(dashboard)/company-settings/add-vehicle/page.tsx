080846a2765064f19c93933baa8900b87cd94231
Montero-MD <matthewdavid.montero@wvsu.edu.ph>
2024-10-26 15:45:58 +0800

    added form validation and changed form width

--- src/app/(pages)/(dashboard)/company-settings/add-vehicle/page.tsx
+++ src/app/(pages)/(dashboard)/company-settings/add-vehicle/page.tsx
@@ -1,13 +1,34 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from 'next/link';
-import Modal from '@/app/components/Modal/Modal';
 import dynamic from "next/dynamic";
-const DynamicMapComponent = dynamic(() => import("@/app/components/Maps/NewPackageMap"), { ssr: false });
+
+interface FormValues {
+  customerName: string;
+  vehicle: string;
+  fuelType: string;
+  consumption: string;
+  foo: string;
+  weight: string;
+  area: string;
+}
+
+interface FormErrors {
+  customerName?: string;
+  vehicle?: string;
+  fuelType?: string;
+  consumption?: string;
+  foo?: string;
+  weight?: string;
+  area?: string;
+}
 
 export default function Page() {
   const [markerCoords, setMarkerCoords] = useState<number[] | null>(null);
-  const [formValues, setFormValues] = useState({
+  const [errors, setErrors] = useState<FormErrors>({});
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [formValues, setFormValues] = useState<FormValues>({
     customerName: '',
     vehicle: '',
     fuelType: '',
@@ -16,6 +37,28 @@ export default function Page() {
     weight: '',
     area: ''
   });
+
+
+  useEffect(() => {
+    if (submitted) {
+      validateForm();
+    }
+  }, [formValues, submitted]);
+
+  // Validate form
+  const validateForm = () => {
+    let errors: { [key: string]: string } = {};
+    if (!formValues.customerName) errors.customerName = 'Name is required.';
+    if (!formValues.vehicle) errors.vehicle = 'Vehicle selection is required.';
+    if (!formValues.fuelType) errors.fuelType = 'Fuel type is required.';
+    if (!formValues.consumption) errors.consumption = 'Consumption is required.';
+    if (!formValues.foo) errors.foo = 'FOO is required.';
+    if (!formValues.weight) errors.weight = 'Weight is required.';
+    if (!formValues.area) errors.area = 'Area is required.';
+
+    setErrors(errors);
+    setIsFormValid(Object.keys(errors).length === 0);
+  };
 
   const handleClear = () => {
     setFormValues({
@@ -27,10 +70,19 @@ export default function Page() {
       weight: '',
       area: ''
     });
+    setErrors({});
+    setSubmitted(false);
   };
 
-  const handleModalToggle = (isOpen: boolean) => {
-    console.log("Modal is", isOpen ? "Open" : "Closed");
+  // Submit
+  const handleSubmit = () => {
+    setSubmitted(true);
+    validateForm();
+    if (isFormValid) {
+      console.log('Form submitted successfully!');
+    } else {
+      console.log('Form has errors. Please correct them.');
+    }
   };
 
   return (
@@ -58,9 +110,9 @@ export default function Page() {
           </div>
         </div>
 
-        {/* Left Side Form */}
+        {/* Form */}
         <div className='flex font-ptsans'>
-          <div className='w-1/3 h-fit flex-none'>
+          <div className='w-full h-fit flex-none'>
             <div className="position-relative bg-white rounded-bl-lg">
               <div className="p-5">
                 <div className="table">
@@ -70,7 +122,7 @@ export default function Page() {
                         <div className="flex items-center justify-start w-full">
                           <h2 className="text-start font-semibold leading-7 text-gray-900 flex-grow">Add Vehicle</h2>
                           <button
-                            className="w-1/2 bg-indigo-100 rounded-[40px] text-neutral-800 text-base font-bold font-roboto py-2 px-4"
+                            className="w-fit bg-indigo-100 rounded-[40px] text-neutral-800 text-base font-bold font-roboto py-2 px-4"
                             onClick={handleClear}
                           >
                             Clear
@@ -81,21 +133,24 @@ export default function Page() {
                           <div className="col-span-full">
                             <div className="mt-2">
                               <input
+                                required
                                 type="text"
-                                name="customer-name"
-                                id="customer-name"
-                                autoComplete="customer-name"
+                                name="customerName"
+                                id="customerName"
+                                autoComplete="customerName"
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-5"
                                 placeholder='Name'
                                 value={formValues.customerName}
                                 onChange={(e) => setFormValues({ ...formValues, customerName: e.target.value })}
                               />
+                              {submitted && errors.customerName && <p className="text-red-500">{errors.customerName}</p>}
                             </div>
                           </div>
 
                           <div className="sm:col-span-3">
                             <div className="mt-2">
                               <select
+                                required
                                 name="vehicle"
                                 id="vehicle"
                                 autoComplete="vehicle"
@@ -108,15 +163,17 @@ export default function Page() {
                                 <option value="truck">Truck</option>
                                 <option value="van">Van</option>
                               </select>
+                              {submitted && errors.vehicle && <p className="text-red-500">{errors.vehicle}</p>}
                             </div>
                           </div>
 
                           <div className="sm:col-span-3">
                             <div className="mt-2">
                               <select
-                                name="fuel-type"
-                                id="fuel-type"
-                                autoComplete="fuel-type"
+                                required
+                                name="fuelType"
+                                id="fuelType"
+                                autoComplete="fuelType"
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-5"
                                 style={{ height: '2.3rem' }}
                                 value={formValues.fuelType}
@@ -126,11 +183,14 @@ export default function Page() {
                                 <option value="diesel">Diesel</option>
                                 <option value="unleaded">Unleaded</option>
                               </select>
+                              {submitted && errors.fuelType && <p className="text-red-500">{errors.fuelType}</p>}
+
                             </div>
                           </div>
                           <div className="sm:col-span-3">
                             <div className="mt-2">
                               <input
+                                required
                                 type="text"
                                 name="consumption"
                                 id="consumption"
@@ -140,11 +200,13 @@ export default function Page() {
                                 value={formValues.consumption}
                                 onChange={(e) => setFormValues({ ...formValues, consumption: e.target.value })}
                               />
+                              {submitted && errors.consumption && <p className="text-red-500">{errors.consumption}</p>}
                             </div>
                           </div>
                           <div className="sm:col-span-3">
                             <div className="mt-2">
                               <input
+                                required
                                 type="text"
                                 name="foo"
                                 id="foo"
@@ -154,6 +216,7 @@ export default function Page() {
                                 value={formValues.foo}
                                 onChange={(e) => setFormValues({ ...formValues, foo: e.target.value })}
                               />
+                              {submitted && errors.foo && <p className="text-red-500">{errors.foo}</p>}
                             </div>
                           </div>
                         </div>
@@ -164,6 +227,7 @@ export default function Page() {
                           <div className="sm:col-span-3">
                             <div className="mt-2">
                               <input
+                                required
                                 type="text"
                                 name="weight"
                                 id="weight"
@@ -173,11 +237,13 @@ export default function Page() {
                                 value={formValues.weight}
                                 onChange={(e) => setFormValues({ ...formValues, weight: e.target.value })}
                               />
+                              {submitted && errors.weight && <p className="text-red-500">{errors.weight}</p>}
                             </div>
                           </div>
                           <div className="sm:col-span-3">
                             <div className="mt-2">
                               <input
+                                required
                                 type="text"
                                 name="area"
                                 id="area"
@@ -187,20 +253,22 @@ export default function Page() {
                                 value={formValues.area}
                                 onChange={(e) => setFormValues({ ...formValues, area: e.target.value })}
                               />
+                              {submitted && errors.area && <p className="text-red-500">{errors.area}</p>}
                             </div>
                           </div>
                         </div>
                       </div>
-                      <Modal onToggle={handleModalToggle} />
+                      <div className="flex justify-center items-center">
+                        {/* Submit Button */}
+                        <button onClick={handleSubmit} type="submit" className="w-fit h-fit align-center bg-indigo-100 rounded-[40px] text-neutral-800 text-base font-bold font-roboto py-2 px-4">
+                          Submit Package
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          {/* Right Side Map */}
-          <div className="w-2/3 h-fit flex">
-            <DynamicMapComponent onMarkerChange={(coords) => setMarkerCoords(coords)} />
           </div>
         </div>
       </div>
