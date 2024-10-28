657eb47e0481fddbb2b9941025dcf6419e9cf130
Montero-MD <matthewdavid.montero@wvsu.edu.ph>
2024-10-29 00:22:38 +0800

    searchbar implementation

--- src/app/ui/tables/searchbar.tsx
+++ src/app/ui/tables/searchbar.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { CiSearch } from "react-icons/ci";
 
+interface SearchBarProps {
+  query: string;
+  setQuery: (query: string) => void;
+}
 
-const SearchBar: React.FC = () => {
+const SearchBar: React.FC<SearchBarProps> = ({ query, setQuery }) => {
   return (
     <div className="searchbar font-ptsans">
-        <CiSearch size={24} className='pr-2'/>
+      <CiSearch size={24} className='pr-2' />
       <input
         type="text"
         placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)} // Update the query on input change
       />
     </div>
   );
